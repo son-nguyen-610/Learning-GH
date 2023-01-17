5f9362ffbf1e73ecd2826619f3fa6178b738c3c9
son-nguyen-610 <114960736+son-nguyen-610@users.noreply.github.com>
2023-01-16 22:32:01 -0600

    Hello to Wassup

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello lmao!")
+            Text("Wassup lmao!")
         }
         .padding()
     }
