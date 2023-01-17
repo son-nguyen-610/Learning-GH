019b887ddc2bc48fc47f3fc29a0e394aa9cc2945
Son Nguyen <ngchoangson@gmail.com>
2023-01-16 21:37:29 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Sơn Nguyễn on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
